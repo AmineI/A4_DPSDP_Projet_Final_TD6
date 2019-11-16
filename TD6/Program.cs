e6a706d7a0c77ed8a89a1198f17acd74d5df1d42
Amine A <amine-98@live.fr>
2019-11-16 23:31:12 +0100

    Le Main appelle la fonction Jeu.LancerPartie

--- TD6/Program.cs
+++ TD6/Program.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TD6
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ReadKey(); //pour empecher la console de se fermer seule
+            Console.WriteLine("Bienvenue dans le Monopoly");
+            do {
+                Jeu.LancerPartie();
+                //Lorsque la partie se termine, l'utilisateur peut choisir s'il en lance une nouvelle ou pas
+            } while (InteractionUtilisateur.ObtenirConfirmation("Voulez vous recommencer une nouvelle partie ?"));
         }
     }
 }
