702602e2a53ebeb53b60bed34523a5d835cf92a3
Amine A <amine-98@live.fr>
2019-11-16 22:25:06 +0100

    Ajout de la classe Dé

---
+++ TD6/De.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD6
+{
+    public static class De
+    {
+        //TODO : Retourner un nombre aléatoire.
+        public static int LancerDe()
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
