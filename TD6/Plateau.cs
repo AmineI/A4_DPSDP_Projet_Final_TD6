9c4ef3944e932c201a010b9e4a59e09b21a82e35
Amine A <amine-98@live.fr>
2019-11-19 16:55:12 +0100

    Plateau is not a singleton anymore

--- TD6/Plateau.cs
+++ TD6/Plateau.cs
@@ -8,42 +8,22 @@ namespace TD6
 {
     public class Plateau
     {
-        /// <summary>
-        /// We store the only instance of a Board in a static variable.
-        /// </summary>
-        static private Plateau board;
-
         private List<Case> boardSpaces;
         public Case this[int key]
         {
             get => boardSpaces[key];
         }
 
-        /// <summary>
-        /// We provide access to our unique Board
-        /// </summary>
-        static public Plateau Board
-        {
-            get => board;
-        }
+        //TODO : Si on ajoute la cagnotte au centre, la stocker sur le plateau
 
-        /// <summary>
-        /// The Board constructor is private so that we can't create a new Board : It ensures the uniqueness of our Board.
-        /// </summary>
-        private Plateau()
+        public Plateau()
         {
             boardSpaces = new List<Case>();
         }
 
-        /// <summary>
-        /// Initialize or reinitialize the board and its spaces
-        /// </summary>
-        public static void Initialize()
-        {
-            board = new Plateau();
-            //TODO : créer et ajouter toutes les cases du plateau à la liste de cases.
-            //TODO : Builder pattern maybe ?
-        }
+        //TODO : créer et ajouter toutes les cases du plateau à la liste de cases.
+        //TODO : Builder pattern maybe ?
+
         //TODO implémenter un indexer pour acceder aux cases du plateau
         //TODO ? Implémenter un enumerator/iterator pour iterer sur le plateau sans avoir accès à la liste de cases ?
 
