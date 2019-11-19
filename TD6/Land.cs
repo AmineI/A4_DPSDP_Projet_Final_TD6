0d20702d48719fd2aadd02fa57323b35d5e11b2a
Kalthurnah <48201356+Kalthurnah@users.noreply.github.com>
2019-11-19 13:01:03 +0100

    Created Land class

---
+++ TD6/Land.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD6
+{
+    class Land : Property
+    {
+        public Land(string id, string name, int buyPrice, int[] rentPrice) : base(id, name, buyPrice, rentPrice)
+        {
+        }
+
+        // TODO : le RentPrice de Land
+        public override int RentPrice
+        {
+            get;
+        }
+    }
+}
