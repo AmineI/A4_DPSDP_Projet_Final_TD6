746d4b8c9692615300d9037929562c206e0f1ac9
Bruleurs <bruleurs@gmail.com>
2019-11-26 17:53:34 +0100

    Implemented jailedPlayer basic functions

--- TD6/JailedPlayer.cs
+++ TD6/JailedPlayer.cs
@@ -1,6 +1,45 @@
 ﻿namespace TD6
 {
-    public class JailedPlayer
+    public class JailedPlayer : IPlayer
     {
+        Player player;
+
+        public int DiceValue => player.DiceValue;
+
+        public bool IsDiceDouble => player.IsDiceDouble;
+
+        public void RollDices()
+        {
+            player.RollDices();
+        }
+
+        public void Earn(int amount)
+        {
+            player.Earn(amount);
+        }
+
+        public void Pay(int amount, IPlayer destinationPlayer)
+        {
+            player.Pay(amount, destinationPlayer);
+        }
+
+        public void PlayTurn()
+        {
+            RollDices();
+            //TODO
+            //
+            // Si Double :
+            //      Sort de prison et continue sur un tour "normal" avec son lancer de dé
+            // Si pas double
+            //      Sort si tour 3
+            //
+            if (IsDiceDouble)
+            {
+                //TODO
+            }
+        }
+
+        
+
     }
 }
