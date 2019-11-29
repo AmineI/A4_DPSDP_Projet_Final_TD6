95826619773323229cd82ade5f5e1e75a7a4df03
Kalthurnah <48201356+Kalthurnah@users.noreply.github.com>
2019-11-29 18:54:12 +0100

    Corrected a mistake in the rules

--- TD6/Spaces/Land.cs
+++ TD6/Spaces/Land.cs
@@ -45,7 +45,7 @@ namespace TD6
                     buildable = false;
                 }
 
-                if ((land.NumberOfHouses != numberOfHouses) && (land.NumberOfHouses != numberOfHouses +1))
+                if ((land.NumberOfHouses != numberOfHouses) && (land.NumberOfHouses+1 != numberOfHouses))
                 {
                     buildable = false;
                 }
