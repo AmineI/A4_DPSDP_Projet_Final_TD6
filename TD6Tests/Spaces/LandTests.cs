c1cdfb78000b8f9c6952dafe45e1fdaf38398c2d
Kalthurnah <48201356+Kalthurnah@users.noreply.github.com>
2019-11-30 16:31:59 +0100

    Created unit tests on land

---
+++ TD6Tests/Spaces/LandTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TD6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD6.Tests
+{
+    [TestClass()]
+    public class LandTests
+    {
+        [TestMethod()]
+        public void LandTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getRentPriceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuildHouseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CanBeSoldTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
