866384fb3ac986932d7425de090506bdcd295452
Bruleurs <46087521+Bruleurs@users.noreply.github.com>
2019-12-01 13:20:32 +0100

    Added GetOutOfJail code

--- TD6/Player.cs
+++ TD6/Player.cs
@@ -188,6 +188,7 @@ namespace TD6
                 {
                     doubleCount = 0;
                     //TODO Go to jail
+                    GetJailed();
                 }
             }
             Move(DiceValue);
