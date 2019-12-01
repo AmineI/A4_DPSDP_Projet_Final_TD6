2224428e2d2eb01597fbd9738d7cdd65112b2a91
Amine I <amine.98@hotmail.fr>
2019-12-01 19:21:22 +0100

    Moved the color before all the prices in the constructor
    May help us later when we will have to generate many spaces

--- TD6/Spaces/Land.cs
+++ TD6/Spaces/Land.cs
@@ -16,7 +16,7 @@ namespace TD6
         public int NumberOfHouses { get => numberOfHouses; }
         public int housePrice;
 
-        public Land(string id, string name, int buyPrice, int[] rentPrices, Color color, int housePrice) : base(id, name, buyPrice, rentPrices)
+        public Land(string id, string name, Color color, int buyPrice, int[] rentPrices, int housePrice) : base(id, name, buyPrice, rentPrices)
         {
             this.housePrice = housePrice;
             this.color = color;
