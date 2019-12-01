c0f6d78ffcd8e0f56a71a8cfdc275dc1591277c5
Amine I <amine.98@hotmail.fr>
2019-12-01 19:59:10 +0100

    Added a IsInMonopoly property for the lands
    Extracted out of GetRentPrice().

--- TD6/Spaces/Land.cs
+++ TD6/Spaces/Land.cs
@@ -30,27 +30,44 @@ namespace TD6
 
         ///A land can be sold only if there is no house on it
         public override bool CanBeSold => numberOfHouses != 0;
+
+
+        /// <summary>
+        /// A boolean indicating if this land is in a monopoly (ie all lands of that color group are owned by the same player) or not
+        /// </summary>
+        public bool IsInMonopoly { get => IsColorMonopolized(this.color); }
+
         /// <summary>
         /// This function gives the rent of the land based on the number of houses and land owned by the land owner.
         /// </summary>
         /// <returns> An integer representing the price of rent</returns>
         public int GetRentPrice()
         {
+            //We get the base rent price according to the number of houses
             int rentPrice = rentPrices[numberOfHouses];
-            List<Land> sameColorLands = Game.Instance.Board.FindAllSpaces<Land>(land => land.Color.Equals(color));
-            bool sameOwner = true;
-            foreach (Land land in sameColorLands)
-            {
-                if (land.Owner != this.Owner)
-                {
-                    sameOwner = false;
-                }
-            }
-            if (sameOwner && numberOfHouses == 0)
-            {
-                rentPrice = rentPrice * 2;
+
+            if (this.IsInMonopoly && this.numberOfHouses == 0)
+            {//If a land is in a monopoly, the rent is doubled on all unimproved lots of that color group.
+                rentPrice *= 2;
             }
             return rentPrice;
+        }
+
+        /// <summary>
+        /// Check if a given color group is monopolized, ie all lands of that color group are owned by the same player
+        /// </summary>
+        /// <param name="color">Color of the group we want to check</param>
+        /// <returns>true if the color group is monopolized (owned by the same player), false otherwise.</returns>
+        public static bool IsColorMonopolized(Color color)
+        {
+            //We gather the list of lands from that color group.
+            List<Land> sameColorLands = Game.Instance.Board.FindAllSpaces<Land>(land => land.Color == color);
+
+            //We get the owner of the first land of that color.
+            IPlayer firstLandOwner = sameColorLands.First<Land>().Owner;
+
+            //And then check if he owns all the lands of that color. If he does, the color is in a monopoly.
+            return sameColorLands.All(land => land.Owner == firstLandOwner);
         }
 
         // TODO : vérifier que le owner est le joueur
