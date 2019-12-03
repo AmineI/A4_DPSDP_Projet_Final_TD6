b298b3a6536518d1288d443b99ad2855faa36d93
Amine I <amine.98@hotmail.fr>
2019-12-03 18:46:45 +0100

    First draft of a ISpaceFactory interface
    Implement by DefaultInternationalSpaceFactory

---
+++ TD6/Creators/DefaultInternationalSpaceFactory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TD6
+{
+    /// <summary>
+    /// Our default space factory according to the international street names and prices.
+    /// </summary>
+    public class DefaultInternationalSpaceFactory : ISpaceFactory
+    {
+        //TODO Factory methods for the other spaces, to write in the Interface
+
+        private static Dictionary<string, SpaceFactoryFunction> PropertyCreators = new Dictionary<string, SpaceFactoryFunction>
+        {
+            ["Old Kent Road"] = (id) => new Land(id, "Old Kent Road", Color.Brown, 60, new[] { 2, 10, 30, 90, 160, 250 }, 50),
+            ["Whitechapel Road"] = (id) => new Land(id, "Whitechapel Road", Color.Brown, 80, new[] { 4, 20, 60, 180, 320, 450 }, 50)
+            //TODO : create the property creators for each property
+        };
+
+        //Store the actions we'll make the event spaces do as static delegates in the factory.
+        public static Action<IPlayer> PassGo = delegate (IPlayer player) { player.Earn(1500); };
+
+
+        public static Action<IPlayer> PayIncomeTax = delegate (IPlayer player) { player.Pay(200); };
+
+
+
+
+        //TODO : Maybe the visit Jail event should display a message on walk.
+
+        public IVisitableSpace CreateProperty(string id, string nameOfThePropertyToCreate)
+        {
+            IVisitableSpace newCreatedSpace = PropertyCreators[nameOfThePropertyToCreate](id);
+            if (newCreatedSpace == null)
+            {
+                throw new ArgumentOutOfRangeException(nameOfThePropertyToCreate, "There is no information about this property in this factory.");
+            }
+            return newCreatedSpace;
+        }
+
+        public IVisitableSpace CreateGoSpace(string id = Constants.GO_SPACE_ID)
+        {
+            return new EventSpace(id, "Go", onStopAction: null, onWalkAction: PassGo);
+        }
+
+        public IVisitableSpace CreateJailSpace(string id = Constants.JAIL_SPACE_ID)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public IVisitableSpace CreateGoToJailSpace(string id = Constants.GO_TO_JAIL_SPACE_ID)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+        public IVisitableSpace CreateParkingSpace(string id = "PARKING")
+        {
+            //TODO If time remains. For now, a parking space does nothing
+            return new EventSpace(id, "Parking (Not Implemented)", onStopAction: null);
+        }
+
+        public IVisitableSpace CreateCommunityChest(string id)
+        {
+            //TODO if time remains. For now, a community chest does nothing.
+            return new EventSpace(id, "Community Chest (Not Implemented)", onStopAction: null);
+        }
+
+        public IVisitableSpace CreateIncomeTax(string id)
+        {
+            return new EventSpace(id, "Income Tax", PayIncomeTax);
+        }
+
+    }
+}
---
+++ TD6/Creators/ISpaceFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD6
+{
+    /// <summary>
+    /// Delegate used to store a function generating a space.
+    /// </summary>
+    /// <param name="id">id to give to the generated space.</param>
+    /// <returns>A new instance of a predefined space, with the specified id.</returns>
+    public delegate IVisitableSpace SpaceFactoryFunction(string id);
+
+    public interface ISpaceFactory
+    {
+        IVisitableSpace CreateProperty(string id, string name);
+        IVisitableSpace CreateGoSpace(string id = Constants.GO_SPACE_ID);
+        IVisitableSpace CreateJailSpace(string id = Constants.JAIL_SPACE_ID);
+        IVisitableSpace CreateGoToJailSpace(string id = Constants.GO_TO_JAIL_SPACE_ID);
+        IVisitableSpace CreateParkingSpace(string id = Constants.PARKING_SPACE_ID);
+        IVisitableSpace CreateCommunityChest(string id);
+        IVisitableSpace CreateIncomeTax(string id);
+
+        //TODO Add other spaces types
+
+    }
+}
