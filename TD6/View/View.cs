503bc15f68a775d5bd4e6e6b41e0a7d036815b5a
Kalthurnah <48201356+Kalthurnah@users.noreply.github.com>
2019-12-08 16:12:33 +0100

    Created View

---
+++ TD6/View/View.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD6.View
+{
+    public class View : IView
+    {
+        /// <summary>
+        /// Ask the player if he wants to buy the property
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns> A boolean representing the response of the player</returns>
+        public bool ConfirmationPurchase(Property property)
+        {
+            bool confirmation = UserInteraction.GetConfirmation("Do you want to buy " + property.Name + " ?");
+            return confirmation;
+        }
+
+        /// <summary>
+        /// Ask the player if he wants to sale the property
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns> A boolean representing the response of the player</returns>
+        public bool ConfirmationSale(Property property)
+        {
+            bool confirmation = UserInteraction.GetConfirmation("Do you want to sale " + property.Name + " ?");
+            return confirmation;
+        }
+    }
+}
