1fbb974ed37487363d81b5da2f8e37c489e0f1bc
Kalthurnah <48201356+Kalthurnah@users.noreply.github.com>
2019-12-08 16:07:38 +0100

    Created IView

---
+++ TD6/View/IView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD6.View
+{
+    public interface IView
+    {
+        void ConfirmationSale(Property property);
+        void ConfirmationPurchase(Property property);
+
+    }
+}
