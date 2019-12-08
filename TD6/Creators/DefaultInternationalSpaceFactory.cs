136a61949fc049e2cdbf2c158ee576f88eb5e221
Amine I <amine.98@hotmail.fr>
2019-12-08 19:56:22 +0100

    Added a few spaces and utilities to the Default International space factory

--- TD6/Creators/DefaultInternationalSpaceFactory.cs
+++ TD6/Creators/DefaultInternationalSpaceFactory.cs
@@ -15,20 +15,29 @@ namespace TD6
             ["Old Kent Road"] = (id, board) => new Land(id, "Old Kent Road", Color.Brown, 60, new[] { 2, 10, 30, 90, 160, 250 }, 50, board),
             ["Whitechapel Road"] = (id, board) => new Land(id, "Whitechapel Road", Color.Brown, 80, new[] { 4, 20, 60, 180, 320, 450 }, 50, board),
             ["Kings Cross Station"] = (id, board) => new Railroad(id, "Kings Cross Station", 200, new[] { 25, 50, 100, 200 }, board),
-            ["The Angel Islington"] = (id, board) => new Land(id, "The Angel Islington", Color.Cyan, 100, new[] { 30,  90,  270, 400, 550 }, 50, board),
+
+            ["The Angel Islington"] = (id, board) => new Land(id, "The Angel Islington", Color.Cyan, 100, new[] { 30, 90, 270, 400, 550 }, 50, board),
             ["Euston Road"] = (id, board) => new Land(id, "Euston Road", Color.Cyan, 100, new[] { 30, 90, 270, 400, 550 }, 50, board),
-            ["Pentonville Road"] = (id, board) => new Land(id, "Pentonville Road", Color.Cyan, 120, new[] { 40,  100, 300, 450, 600 }, 50, board),
+            ["Pentonville Road"] = (id, board) => new Land(id, "Pentonville Road", Color.Cyan, 120, new[] { 40, 100, 300, 450, 600 }, 50, board),
+
             ["Pall Mall"] = (id, board) => new Land(id, "Pall Mall", Color.Purple, 140, new[] { 50, 150, 450, 625, 750 }, 100, board),
-            //TODO : compagnie case
+            ["Electric Company"] = (id, board) => new Utility(id, "Electric Company", 150, new[] { 4, 10 }, board),
             ["Whitehall"] = (id, board) => new Land(id, "Whitehall", Color.Purple, 140, new[] { 50, 150, 450, 625, 750 }, 100, board),
             ["Northumberland Avenue"] = (id, board) => new Land(id, "Northumberland Avenue", Color.Purple, 160, new[] { 60, 180, 500, 700, 900 }, 100, board),
+
             ["Marylebone Station"] = (id, board) => new Railroad(id, "Marylebone Station", 200, new[] { 25, 50, 100, 200 }, board),
             ["Bow Street"] = (id, board) => new Land(id, "Bow Street", Color.Orange, 180, new[] { 70, 200, 550, 750, 950 }, 100, board),
             ["Marlborough Street"] = (id, board) => new Land(id, "Marlborough Street", Color.Orange, 180, new[] { 70, 200, 550, 750, 950 }, 100, board),
             ["Vine Street"] = (id, board) => new Land(id, "Vine Street", Color.Orange, 200, new[] { 80, 220, 600, 800, 1000 }, 100, board),
-            ["The Strand"] = (id, board) => new Land(id, "The Strand", Color.Red, 220, new[] { 90, 250, 700, 875, 1050 }, 100, board),
-            ["Fleet Street"] = (id, board) => new Land(id, "Fleet Street", Color.Red, 220, new[] { 90, 250, 700, 875, 1050 }, 100, board),
-            ["Trafalgar Square"] = (id, board) => new Land(id, "Trafalgar Square", Color.Red, 240, new[] { 100, 300, 750, 925, 1100 }, 100, board)
+
+            ["The Strand"] = (id, board) => new Land(id, "The Strand", Color.Red, 220, new[] { 90, 250, 700, 875, 1050 }, 150, board),
+            ["Fleet Street"] = (id, board) => new Land(id, "Fleet Street", Color.Red, 220, new[] { 90, 250, 700, 875, 1050 }, 150, board),
+            ["Trafalgar Square"] = (id, board) => new Land(id, "Trafalgar Square", Color.Red, 240, new[] { 100, 300, 750, 925, 1100 }, 150, board),
+            ["Fenchurch St Station"] = (id, board) => new Railroad(id, "Fenchurch St Station", 200, new[] { 25, 50, 100, 200 }, board),
+            //TODO : Fix above by adding the site prices, without any house
+            ["Leicester Square"] = (id, board) => new Land(id, "Leicester Square", Color.Yellow, 260, new[] { 22, 110, 330, 800, 975, 1150 }, 150, board),
+            ["Coventry Street"] = (id, board) => new Land(id, "Coventry Street", Color.Yellow, 260, new[] { 22, 110, 330, 800, 975, 1150 }, 150, board),
+            ["Water Works"] = (id, board) => new Utility(id, "Water Works", 150, new[] { 4, 10 }, board),
             //TODO : create the property creators for each property
         };
 
