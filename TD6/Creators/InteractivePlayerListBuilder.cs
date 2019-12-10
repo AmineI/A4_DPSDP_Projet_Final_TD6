625b73c4de455ac6535c9fe92d582e387bdd2ce2
Amine I <amine.98@hotmail.fr>
2019-12-10 13:51:33 +0100

    Created InteractivePlayerListBuilder

---
+++ TD6/Creators/InteractivePlayerListBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD6.Creators
+{
+    static class InteractivePlayerListBuilder
+    {
+        static IList<Player> AskForPlayerList()
+        {
+            IList<Player> players = new List<Player>();
+            //Add players
+
+            return players;
+
+        }
+    }
+}
