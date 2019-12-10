dec633ba6aa5c0d05091420282964680a58bfea4
Amine I <amine.98@hotmail.fr>
2019-12-10 14:20:46 +0100

    PlayerFactory CreatePlayer method

---
+++ TD6/Creators/PlayerFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD6
+{
+    static class PlayerFactory
+    {
+        private static int id = 0;
+        public static Player CreatePlayer(string playerName, Game gameInstance = null)
+        {
+            id++;
+            return new Player(id, playerName, 1500, (char)(id+9785), gameInstance);//9785 is where the common symbols like ☺ start.
+        }
+    }
+}
