c698c0267aedca26ccd2f46b6f3d55689986b295
Amine I <amine.98@hotmail.fr>
2019-12-14 23:28:40 +0100

    Changed the symbol to a letter.

--- TD6/Creators/PlayerFactory.cs
+++ TD6/Creators/PlayerFactory.cs
@@ -12,7 +12,7 @@ namespace TD6
         public static Player CreatePlayer(string playerName, IGame gameInstance = null)
         {
             id++;
-            return new Player(id, playerName, 1500, (char)(id+13), gameInstance);//13 is where common symbols start
+            return new Player(id, playerName, 1500, (char)(id+64), gameInstance);//13 is where common symbols start
         }
     }
 }
